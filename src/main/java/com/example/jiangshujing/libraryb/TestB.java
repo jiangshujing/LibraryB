e0a8202161de5b4f209efe94a55f3270962e7040
jiangshujing <jiangshujing@teddymobile.cn>
2019-06-04 15:51:47 +0800

    添加 set方法

--- src/main/java/com/example/jiangshujing/libraryb/TestB.java
+++ src/main/java/com/example/jiangshujing/libraryb/TestB.java
@@ -2,7 +2,14 @@ package com.example.jiangshujing.libraryb;
 
 public class TestB {
 
+    static String name;
+
     public static String get() {
         return "LibraryB";
+    }
+
+
+    public static void set(String name) {
+        TestB.name = name;
     }
 }
